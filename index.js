09ba2890538c59a199d091f03bca19bcf99e03bf
Jalen Peeler <jalenpeeler@yahoo.com>
2021-04-03 01:42:52 -0400

    Done control flow

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let feet;
+  if (ride <= 400) {
+    feet = "This one is on me!";
+  } else if (ride > 2000 && ride <= 2500) {
+    feet = "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+  return feet;
+} 
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city){
+  const check = "NYC";
+  return city === check ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much.";
+      break;
+    case "not as generous" :
+      return "Thank you."
+      break;
+    default :
+      return "Bye."
+  }
 }
